f3b9c864fa5835490096281cb9001c35ef4cfeb6
IvanMorenoRuiz <443948.joan23@fje.edu>
2024-04-04 20:01:30 +0200

    MapasCheck

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UbicacionController;
+
 
 Route::get('/admin', function () {
     return view('admin.index');
@@ -21,5 +23,9 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::post('/guardar-ubicacion', [UbicacionController::class, 'guardarUbicacion'])->name('guardar-ubicacion');
+
+
 
 require __DIR__.'/auth.php';
