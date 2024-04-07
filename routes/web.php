e2f85c2040ed4d5a41c8228edf317aee53ce03e0
Wirson02 <alvarracinalfredo@gmail.com>
2024-04-07 21:09:03 +0200

    Login Funcional

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,10 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware('guest')->group(function () {
+    Route::get('/', function () { return view('auth.login');});
 });
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -16,5 +17,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+
 
 require __DIR__.'/auth.php';
